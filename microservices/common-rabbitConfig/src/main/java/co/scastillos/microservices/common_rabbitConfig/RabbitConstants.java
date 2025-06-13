e3db09db7d00409a7d1ae4f6ae1db440fd02e448
SaantiagoAlonso <castillosantiago442@gmail.com>
2025-06-12 23:25:50 -0500

    add common-configRabbit, description of exchange and queue

---
+++ microservices/common-rabbitConfig/src/main/java/co/scastillos/microservices/common_rabbitConfig/RabbitConstants.java
@@ -0,0 +1,21 @@
+package co.scastillos.microservices.common_rabbitConfig;
+
+public class RabbitConstants {
+
+    public static final String USER_EXCHANGE = "user.exchange";
+    public static final String USER_QUEUE = "user.queue";
+    public static final String USER_ROUTING_KEY = "user.routing.key";
+
+    public static final String STUDENT_EXCHANGE = "student.exchange";
+    public static final String STUDENT_QUEUE = "student.queue";
+    public static final String STUDENT_ROUTING_KEY = "student.routing.key";
+
+    public static final String TEACHER_EXCHANGE = "teacher.exchange";
+    public static final String TEACHER_QUEUE = "teacher.queue";
+    public static final String TEACHER_ROUTING_KEY = "teacher.routing.key";
+
+    public static final String CURSE_EXCHANGE = "curse.exchange";
+    public static final String CURSE_QUEUE = "curse.queue";
+    public static final String CURSE_ROUTING_KEY = "curse.routing.key";
+
+}
